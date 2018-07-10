5decfaead1a90fd371183cc1153bd1772c788ba6
Tory Dannenberg <rfdberg@gmail.com>
2018-07-10 16:43:18 -0600

    Setup reducer index file

--- src/reducers/index.js
+++ src/reducers/index.js
@@ -1,2 +1,13 @@
-// combine reducers 
-// establish state tree
+import { combineReducers } from 'redux';
+import { moviesReducer } from './moviesReducer';
+import { favoritesReducer } from './favoritesReducer';
+import { usersReducer } from './usersReducer';
+
+const rootReducer = combineReducers({
+  movies: moviesReducer,
+  favorites: favoritesReducer,
+  users: usersReducer
+});
+
+
+export default rootReducer;
