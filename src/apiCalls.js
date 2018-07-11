0f0aef6721003e7e3262399eeed839b7246d505d
Jeremiah Stanley <jeremiah.j.stanley@gmail.com>
2018-07-11 17:42:56 -0600

    Change file structure to use modular architecture

--- src/apiCalls.js
+++
@@ -1,9 +0,0 @@
-
-const fetchMovieData = async (key) => {
-  const url = `https://api.themoviedb.org/3/discover/movie?api_key=${key}&language=en-US&sort_by=popularity.desc&include_adult=false&include_video=false&page=1`;
-  const response = await fetch(url);
-  const results = await response.json();
-  return results;
-};
-
-export default fetchMovieData;
