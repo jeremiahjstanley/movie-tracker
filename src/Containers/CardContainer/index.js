066ed97f972e8b92a5b1e0e8975d007858c4d010
Spenser Leighton <spenser.leighton@gmail.com>
2018-07-11 11:00:36 -0600

    Refactor App and CardContainer

--- src/Containers/CardContainer/index.js
+++ src/Containers/CardContainer/index.js
@@ -1,26 +1,44 @@
-import React from 'react';
-import './styles.css';
+import React, { Component } from 'react';
+import fetchMovieData from '../../apiCalls';
+import { apiKey } from '../../apiKey';
+import { movieCleaner } from '../../helper';
 import { connect } from 'react-redux';
 import { Link } from 'react-router-dom';
-import Card from '../Card';
+import Card from '../../Components/Card';
+import { addMovies } from '../../actions';
 
-const CardContainer = () => {
-  const moviesToDisplay = props.movies.map(movie => {
-    return <Link to={`/movies/${movie.title}`}>
-              <Card {...movie} />
-           </Link>
-  });
+class CardContainer extends Component {
 
-  return (
-    <div>
-      { moviesToDisplay }
-    </div>
-  );
+  componentDidMount = async () => {
+    const movies = await fetchMovieData(apiKey);
+    const cleanMovies = await movieCleaner(movies);
+    this.props.addMovies(cleanMovies);
+  }
+
+  moviesToDisplay = ({ movies }) => {
+    return movies.map(movie => {
+      console.log(movie)
+    }); 
+  } 
+
+
+
+  render() {
+    return (
+      <div>
+
+      </div>
+    );
+  }
 }
 
 export const mapStateToProps = (state) => ({
   movies: state.movies
 });
 
+export const mapDispatchToProps = (dispatch) => ({
+  addMovies: (movies) => dispatch(addMovies(movies))
+});
 
-export default connect(mapStateToProps)(CardContainer);
+
+export default connect(mapStateToProps,mapDispatchToProps)(CardContainer);
