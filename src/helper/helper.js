9de477a735e059dd63b85e4b30de04d7d357b6a1
Spenser Leighton <spenser.leighton@gmail.com>
2018-07-13 22:28:36 -0600

    Refactor helper data cleaner

--- src/helper/helper.js
+++ src/helper/helper.js
@@ -1,14 +1,13 @@
 export const movieCleaner = response => {
   const movies = response.results.map(result => ({
     id: result.id,
-    backdrop: result.backdrop_path,
-    favorite: false,
-    movieImage: result.poster_path,
+    backdrop_path: result.backdrop_path,
+    poster_path: result.poster_path,
     overview: result.overview,
-    rating: result.vote_average,
-    releaseDate: result.release_date,
+    vote_average: result.vote_average,
+    release_date: result.release_date,
     tagline: result.tagline,
-    title: result.original_title
+    original_title: result.original_title
   })
   );
   return movies;
