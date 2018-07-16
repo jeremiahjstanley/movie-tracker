87dbe559b15d3005f645aee4d6b5db68499498df
Spenser Leighton <spenser.leighton@gmail.com>
2018-07-16 13:56:41 -0600

    Add test to card container

--- src/Containers/CardContainer/CardContainer.test.js
+++ src/Containers/CardContainer/CardContainer.test.js
@@ -1,7 +1,11 @@
 import React from 'react';
 import { CardContainer } from './index.js';
 import { shallow, mount } from 'enzyme';
+import { fetchMovieData } from '../../helper/apiCalls';
+import { movieCleaner } from '../../helper/helper';
 
+jest.mock('../../helper/apiCalls')
+jest.mock('../../helper/helper')
 
 describe('CardContainer', () => {
   it('should match the snapshot', () => {
@@ -22,4 +26,12 @@ describe('CardContainer', () => {
 
     expect(mockCheckFavorites).toHaveBeenCalled();
   });
+
+  it('when component is mounted, fetchMovieData is called with correct param', async () => {
+    const mockMovies = [{ title: 'ConAir', id: 7, poster_path: 'google.com' }];
+    const mockFavorites = [{ title: 'ConAir', movie_id: 9 }];
+    const wrapper = shallow(<CardContainer movies={mockMovies} favorites={mockFavorites} addMovies={jest.fn()}/>);
+
+    await expect(fetchMovieData).toHaveBeenCalled()
+  })
 });
