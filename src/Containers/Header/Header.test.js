fccca8528550dae5e60e8b2c0dda625da3a1a5e9
Spenser Leighton <spenser.leighton@gmail.com>
2018-07-16 14:46:22 -0600

    Add header tests

---
+++ src/Containers/Header/Header.test.js
@@ -0,0 +1,21 @@
+import React from 'react';
+import { Header } from './index.js';
+import { shallow } from 'enzyme';
+
+describe('Header', () => {
+  let wrapper;
+  let mockLogOut;
+
+  beforeEach(() => {
+    mockLogOut = jest.fn()
+    wrapper = shallow(<Header users={{ id: 7 }} favorites={['Happy birthday Casey Poe']} logOut={ mockLogOut } />)
+  })
+  it('should match the snapshot', () => {
+    expect(wrapper).toMatchSnapshot();
+  })
+
+  it('should call logOut when logOut button is clicked', () => {
+    wrapper.find('.log-out').simulate('click')
+    expect(mockLogOut).toHaveBeenCalled()
+  })
+})
