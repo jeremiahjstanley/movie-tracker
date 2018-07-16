e7dbd090f2dc79602030ef02ab55c8151f9c82d9
Spenser Leighton <spenser.leighton@gmail.com>
2018-07-16 14:29:22 -0600

    Add test to favs container

--- src/Containers/FavoritesContainer/FavoritesContainer.test.js
+++ src/Containers/FavoritesContainer/FavoritesContainer.test.js
@@ -15,13 +15,23 @@ describe('Favorites Container Test', () => {
   it('should render all the cards', () => {
     const mockFavorites = [{ favorite: true, title: 'ConAir' }, { favorite: true, title: 'The Rock' }];
     const wrapper = shallow(<FavoritesContainer favorites={mockFavorites} />);
-    const results = wrapper.find('.favorite').length;
+    const results = wrapper.find('.favorite-card').length;
     
     expect(results).toEqual(2);
   });
 
-  it('should invoke checkFavorites with the movie id when favorite button is clicked', () => {
+  it('should invoke checkFavorites with the movie_id when favorite button is clicked', () => {
     const mockFavorites = [{ favorite: true, title: 'ConAir', movie_id: 5 }];
+    const mockCheckFavorites = jest.fn();
+    const wrapper = shallow(<FavoritesContainer favorites={mockFavorites} checkFavorites={mockCheckFavorites}/>);
+
+    wrapper.find('button').simulate('click');
+
+    expect(mockCheckFavorites).toHaveBeenCalledWith(5);
+  });
+
+  it('should invoke checkFavorites with the id when favorite button is clicked', () => {
+    const mockFavorites = [{ favorite: true, title: 'ConAir', id: 5 }];
     const mockCheckFavorites = jest.fn();
     const wrapper = shallow(<FavoritesContainer favorites={mockFavorites} checkFavorites={mockCheckFavorites}/>);
 
