e2f8a9d7328e39c20fe15bbb9eb995ed803cd283
Spenser Leighton <spenser.leighton@gmail.com>
2018-07-16 13:57:49 -0600

    Add test to card container

--- src/reducers/index.js
+++ src/reducers/index.js
@@ -1,4 +1,5 @@
 import { combineReducers } from 'redux';
+
 import { moviesReducer } from './moviesReducer';
 import { loginReducer } from './loginReducer';
 import { favoritesReducer } from './favoritesReducer';
