455ed922fd4b1730576e7c908abff5413df2c910
Jeremiah Stanley <jeremiah.j.stanley@gmail.com>
2018-07-15 21:14:56 -0600

    Add PropTypes validation to Header container component

--- src/Containers/Header/index.js
+++ src/Containers/Header/index.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react';
 import { NavLink, withRouter } from 'react-router-dom';
 import { connect } from 'react-redux';
+import PropTypes from 'prop-types';
 import logo from './resources/logo.png'
 import './styles.css'
 
@@ -9,7 +10,7 @@ class Header extends Component {
   render() {
     return (
         <header className='app-header'>
-        <img src={logo} className='logo'/>
+        <img src={logo} className='logo' alt='Cage Tracker Logo'/>
         <ul className='navigation-links'>
           <li>
             <NavLink to='/'>
@@ -36,5 +37,11 @@ export const mapStateToProps = (state) => ({
   users: state.login,
   favorites: state.favorites
 });
+
+Header.propTypes = {
+  users: PropTypes.object,
+  favorites: PropTypes.array.isRequired
+};
+
 
 export default withRouter(connect(mapStateToProps)(Header));
