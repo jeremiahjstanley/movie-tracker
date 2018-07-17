76e67e1219f0ea3a487c2d87571eae443aef6ead
Jeremiah Stanley <jeremiah.j.stanley@gmail.com>
2018-07-16 19:27:16 -0600

    Add new tests for LoginForm container component

--- src/Containers/LoginForm/LoginForm.test.js
+++ src/Containers/LoginForm/LoginForm.test.js
@@ -1,19 +1,43 @@
 import React from 'react';
 import { LoginForm, mapStateToProps, mapDispatchToProps } from './index.js';
 import { shallow, mount } from 'enzyme';
-import { BrowserRouter, Link, withRouter } from 'react-router-dom';
+import { BrowserRouter as Router }from 'react-router-dom';
+import { logIn, updateFavorites, getUserFavorites } from '../../actions';
+import { getFavoritesFromDatabase } from '../../helper/apiCalls'
 
+jest.mock('../../helper/apiCalls')
 
 describe('Login Form tests', () => {
-  it('should match the snapshot', () => {
-    const wrapper = shallow(<LoginForm />);
+  let wrapper;
+  let mockGetUserFavorites;
+  let mockLogInUser;
+  let mockHistory;
+  let mockResponse
 
+  beforeEach(() => {
+
+    mockGetUserFavorites = jest.fn();
+    mockLogInUser = jest.fn();
+    mockHistory = [];
+    mockResponse = { 
+      data: {
+        name: 'Nick',
+        email: 'nick@msn.com',
+        id: 2,
+    }}
+    wrapper = shallow(
+      <LoginForm 
+        getUserFavorites={mockGetUserFavorites}
+        logInUser={mockLogInUser} 
+        history={mockHistory}/>
+      );
+  });
+
+  it('should match the snapshot', () => {
     expect(wrapper).toMatchSnapshot();
   });
 
-  it('should have a default state of email, password and error message, all empty strings', () => {
-    const wrapper = shallow(<LoginForm />);
-    const expected = {
+  it('should have a default state of email, password and error message, all empty strings', () => {    const expected = {
       email: '',
       password: '',
       errorMessage: ''
@@ -22,15 +46,13 @@ describe('Login Form tests', () => {
   });
 
   it('should update the state when handleChange is invoked', () => {
-    const wrapper = shallow(<LoginForm />);
-
     const mockEvent = { target: { value: 'NickCage@aol.com', name: 'email' } };
     wrapper.instance().handleChange(mockEvent);
     expect(wrapper.state('email')).toBe('NickCage@aol.com');
   });
 
-  it('should invoke handleChange when the email field changes', () => {
-    const wrapper = mount(withRouter(<LoginForm />), { disableLifecycleMethods: true });
+  it.skip('should invoke handleChange when the email field changes', () => {
+    const wrapper = mount(<Router><LoginForm /></Router>);
     const spy = spyOn(wrapper.instance(), 'handleChange');
     wrapper.instance().forceUpdate();
     const mockEvent = { target: { value: 'NickCage@aol.com', name: 'email' } };
@@ -39,28 +61,63 @@ describe('Login Form tests', () => {
     expect(spy).toHaveBeenCalledWith();
   });
 
-  
+  it('should invoke handleSubmit when the form is submitted', () => {    const spy = spyOn(wrapper.instance(), 'handleSubmit')
+    wrapper.setState({
+      email: 'nick@msn.com',
+      password: 'unleashthecage',
+    })
 
-  // it('should calls handleChange when the password field changes', () => {
-  //   const wrapper = shallow(<LoginForm />);
-  //   const spy = spyOn(wrapper.instance(), 'handleChange');
-  //   wrapper.instance().forceUpdate();
-  //   const mockEvent = { target: { value: 'password', name: 'password' } };
-  //   wrapper.find('.password-input').simulate('change', mockEvent);
+    const mockEvent = { preventDefault: jest.fn() }
 
-  //   expect(spy).toHaveBeenCalled();
-  // });
+    const submitButton = wrapper.find('.login-form')
+    submitButton.simulate('submit', mockEvent)
 
-  // it('should reset state after handle submit is called', () => {
-    
-  //   expect('').toEqual('');
-  // });
+    expect(spy).toHaveBeenCalledWith(mockEvent);
+  });
 
+  it.skip('should invoke storeUser with the correct arguments when the form is submitted', async () => {
+    await wrapper.instance().handleUpdate(mockResponse)
+    // this is where we need to mock local storage //
+
+    expect(mockLogInUser).toHaveBeenCalledWith('nick@msn.com', 'Nick', 2);
+  });
+
+  it('should invoke logInUser with the correct arguments when the form is submitted', async () => {
+    await wrapper.instance().handleUpdate(mockResponse)
+
+    expect(mockLogInUser).toHaveBeenCalledWith('nick@msn.com', 'Nick', 2);
+  });
+
+  it('should fetch the users favorites from the database when the form is submitted', async () => {
+    await wrapper.instance().handleUpdate(mockResponse)
+
+    expect(getFavoritesFromDatabase).toHaveBeenCalledWith(2);
+  });
+
+  it('should invoke getUserFavorites when the form is submitted', async () => {
+    await wrapper.instance().handleUpdate(mockResponse)
+
+    expect(mockGetUserFavorites).toHaveBeenCalled();
+  });
+
+  it('should invoke apply a favorite property to the favorite movies returned from the database when the form is submitted', async () => {
+    const mockArray = [{favorite: true, title: "Con Air"}, {favorite: true, title: "Face/Off"}, {favorite: true, title: "Captain Ron"}]
+
+    await wrapper.instance().handleUpdate(mockResponse)
+
+    expect(mockGetUserFavorites).toHaveBeenCalledWith(mockArray);
+  });
+
+  it('should redirect the user when the form is submitted', async () => {
+    await wrapper.instance().handleUpdate(mockResponse)
+
+    expect(mockHistory).toEqual(['/']);
+  });
 
   describe('mapStateToProps', () => {
     it('should return an object with the email, name and favorites array', () => {
-      const mockState = { "favorites": [{ "title": "Con Air" }], "login": { "email": "nickcage@aol.com", "name": "Nick Cage" } };
-      const expected = { "email": "nickcage@aol.com", "favorites": [{ "title": "Con Air" }], "name": "Nick Cage" };
+      const mockState = { favorites: [{ title: 'Con Air' }], login: { email: 'nickcage@aol.com', name: 'Nick Cage' } };
+      const expected = { email: 'nickcage@aol.com', favorites: [{ title: 'Con Air' }], name: 'Nick Cage' };
       const mappedProps = mapStateToProps(mockState);
 
       expect(mappedProps).toEqual(expected);
@@ -68,7 +125,29 @@ describe('Login Form tests', () => {
   });
 
   describe('mapDispatchToProps', () => {
+    it('calls dispatch when the form is submitted to log the user in', () => { 
+      const mockGetUserFavorites = jest.fn();
+      const mockLogInUser = jest.fn();
 
+      const mockDispatch = jest.fn();
+      const mappedProps = mapDispatchToProps(mockDispatch)
+      const actionToDispatch = logIn('nickcage@aol.com', 'Nick', 1)
+      mappedProps.logInUser('nickcage@aol.com', 'Nick', 1)
+
+      expect(mockDispatch).toHaveBeenCalledWith(actionToDispatch)
+    })
+
+    it('calls dispatch when the form is submitted to retrieve the users favorites', () => { 
+      const mockGetUserFavorites = jest.fn();
+      const mockLogInUser = jest.fn();
+
+      const mockDispatch = jest.fn();
+      const mappedProps = mapDispatchToProps(mockDispatch)
+      const actionToDispatch = updateFavorites(['Con Air'])
+      mappedProps.getUserFavorites(['Con Air'])
+
+      expect(mockDispatch).toHaveBeenCalledWith(actionToDispatch)
+    })
   });
 
 });
