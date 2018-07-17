85b4666649543e86eb6c7d314dbe75e6b3ca8a00
Spenser Leighton <spenser.leighton@gmail.com>
2018-07-16 21:16:15 -0600

    Refactor moviesReducer

--- src/reducers/tests/moviesReducer.test.js
+++ src/reducers/tests/moviesReducer.test.js
@@ -2,9 +2,29 @@ import { moviesReducer } from '../moviesReducer.js';
 import * as actions from '../../actions';
 
 describe('Movies Reducter tests', () => {
+  let initialState;
+
+  beforeEach(() => {
+    initialState = [{ id: 14361,
+        backdrop_path: '/q8yzwvVm9PjusRI2rTG8gGk5xBM.jpg',
+        poster_path: '/rvVQmoGJiMSsi3otft411TtNxdO.jpg',
+        overview: 'When mild-mannered Martin Harvey finds out that he has inherited a vintage yacht, he decides to take his family on a Caribbean vacation to retrieve the vessel. Upon arriving on a small island and realizing that the ship is in rough shape, Martin and his family end up with more than they bargained for as the roguish Captain Ron signs on to sail the boat to Miami. It doesn\'t take long before Ron\'s anything-goes antics get the Harveys into plenty of trouble.',
+        vote_average: 10,
+        release_date: '1992-09-18',
+        tagline: 'Martin just wanted a nice, quiet family vacation. Instead, he got... Captain Ron',
+        title: 'Captain Ron' }]
+  })
 
   it('should return the initial state', () => {
-    const expected = [];
+    const expected = [{ id: 14361,
+        backdrop_path: '/q8yzwvVm9PjusRI2rTG8gGk5xBM.jpg',
+        poster_path: '/rvVQmoGJiMSsi3otft411TtNxdO.jpg',
+        overview: 'When mild-mannered Martin Harvey finds out that he has inherited a vintage yacht, he decides to take his family on a Caribbean vacation to retrieve the vessel. Upon arriving on a small island and realizing that the ship is in rough shape, Martin and his family end up with more than they bargained for as the roguish Captain Ron signs on to sail the boat to Miami. It doesn\'t take long before Ron\'s anything-goes antics get the Harveys into plenty of trouble.',
+        vote_average: 10,
+        release_date: '1992-09-18',
+        tagline: 'Martin just wanted a nice, quiet family vacation. Instead, he got... Captain Ron',
+        title: 'Captain Ron' }];
+
     const results = moviesReducer(undefined, {});
 
     expect(results).toEqual(expected);
@@ -31,16 +51,9 @@ describe('Movies Reducter tests', () => {
         tagline: 'Im gonna take your face... off!',
         title: 'Face Off'
       }];
-    const expected = movies;
+    const expected = [movies[0], movies[1], ...initialState];
     const results = moviesReducer(undefined, actions.addMovies(movies));
 
     expect(results).toEqual(expected);
   });
-
-  // it('should return an email address that is an empty string', () => {
-  //   const expected = { email: '' };
-  //   const results = loginReducer(undefined, actions.logOut());
-
-  //   expect(results).toEqual(expected);
-  // });
 });
