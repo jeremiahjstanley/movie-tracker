f070cd9615067151646e5195bc8ce183638b2f75
Jeremiah Stanley <jeremiah.j.stanley@gmail.com>
2018-07-16 19:26:05 -0600

    Create new mock for getFavoritesFromDatabase and mockImplementation

--- src/helper/__mocks__/apiCalls.js
+++ src/helper/__mocks__/apiCalls.js
@@ -2,3 +2,6 @@ export const fetchMovieData = jest.fn();
 
 export const deleteFavoriteFromDatabase = jest.fn();
 
+export const getFavoritesFromDatabase = jest.fn().mockImplementation(() => ({
+  data: [{title: 'Con Air'}, {title: 'Face/Off'}, {title: 'Captain Ron'}]
+}))
